0571357751a381368b9a93d38ace6505d47075e2
Michael Friedrich <fred_derf204@hotmail.com>
2024-03-08 11:45:10 +1100

    first commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,59 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/azure/azapi" {
+  version     = "0.1.0"
+  constraints = "0.1.0"
+  hashes = [
+    "h1:chcm9s4sRNo1TMJPXIkpyZioEjr5wH1jUQDFkWF0swQ=",
+    "zh:1ce60ee8480f1e3af8a3534eedb6ec28e51adc72967d32f04393d0b033f3bb39",
+    "zh:1e6de8c6a6d7ca85ae4a3124cc1733ac0436a3426fd21c2970c867e269390c06",
+    "zh:1e9a43d975866698bcda3e0e0dd1d18c9b71403a309ece5978507ba640043da7",
+    "zh:356cd346c93cecc429f701a48d301a22ea8137b1358b307d55ec6131ff69efb1",
+    "zh:35e4551806fc9783f003ff55e9b8afb00205492417587b7cb009a6ae8e408882",
+    "zh:369340a3b441dcfca12097de26215b4a1910226cf91f26d0b21b2fe664556f6e",
+    "zh:8dee1b5135ed8898114e859b640028dddb11707ed91077323fae5018e2e375b9",
+    "zh:8ed4627d7d68136ea3af6601aeb31975253d4fbc110c936f4e04dd17c4517657",
+    "zh:a0f609144270ccc03e66726529b8be8bd77b3c80ebf89259625361e61797c354",
+    "zh:ab938e078671745fe1d1ed80ebc1fcfa36b0e1af153421b2dc93f750435ae517",
+    "zh:b7d73d907d8ab57677e4808f7a4787efa024fd7326f3f50fd5ea8123a81cce1f",
+    "zh:fde411ecc0c59e8725d29f71adcc1920826ca0966e8d87bf51cc387e05677196",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.80.0"
+  constraints = "3.80.0, ~> 3.80"
+  hashes = [
+    "h1:Hyl3flDPofpLa0Bi3wTghhuek2fleM96e8ugcC25UHo=",
+    "zh:11e3362a3d9ca9d6d57a3c863b26ddcb9e4366c6c99586a4149d2842976e998c",
+    "zh:3005446ee10379e5354b2388d788cad92068ff0988f50e49f6e8eeacdbd6e0a1",
+    "zh:45aa9f9d930e332fc68f2093232edf15ef0d087c9588317371838784382b9dbc",
+    "zh:4e1d57b6b909577b37e85117533eb0d6a2128e7ccfcf88e709ad890f86e67295",
+    "zh:5f777dbb7d72ab83a3a9b9412f8e341573fb34591c834fefa8fc7593e73232a2",
+    "zh:6fab9ea69a2177e29a0c0819f32c51245891fd19b807c4a3e2d4dd5c0a28b8fb",
+    "zh:77b1bdf671a83e17c82b30520312970109811c7725b61fcef69138244d7dd212",
+    "zh:8a1e8d9243f1149b7bcf7b8bda3cf5a1bcea6c709b55053fe7e8cd767dd5f632",
+    "zh:a7c1de65d5f1f241c4d3f30f24912cb9dc8315a1dc7d7bd0814cb4beb1e8f1fb",
+    "zh:b4385e7c84d708cd81647555dd12ade9fb128fd027eb59cd651f4cdcf98a437e",
+    "zh:d4ef3f5b126d5c9766300ffb6f1b3ed091484d46c742cfae2645261e749162e5",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/template" {
+  version = "2.2.0"
+  hashes = [
+    "h1:N2IXrfDhxid5pFKHISN9V23ptHJyFZEdojUegdx5YkM=",
+    "zh:01702196f0a0492ec07917db7aaa595843d8f171dc195f4c988d2ffca2a06386",
+    "zh:09aae3da826ba3d7df69efeb25d146a1de0d03e951d35019a0f80e4f58c89b53",
+    "zh:09ba83c0625b6fe0a954da6fbd0c355ac0b7f07f86c91a2a97849140fea49603",
+    "zh:0e3a6c8e16f17f19010accd0844187d524580d9fdb0731f675ffcf4afba03d16",
+    "zh:45f2c594b6f2f34ea663704cc72048b212fe7d16fb4cfd959365fa997228a776",
+    "zh:77ea3e5a0446784d77114b5e851c970a3dde1e08fa6de38210b8385d7605d451",
+    "zh:8a154388f3708e3df5a69122a23bdfaf760a523788a5081976b3d5616f7d30ae",
+    "zh:992843002f2db5a11e626b3fc23dc0c87ad3729b3b3cff08e32ffb3df97edbde",
+    "zh:ad906f4cebd3ec5e43d5cd6dc8f4c5c9cc3b33d2243c89c5fc18f97f7277b51d",
+    "zh:c979425ddb256511137ecd093e23283234da0154b7fa8b21c2687182d9aea8b2",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,249 @@
+# Variables
+variable "aoai-service-name-11" {
+  type = string
+  description = "Name of the first AOAI service in Prority 1 group"
+  default = "can-aoai-smart-11"
+  
+}
+
+variable "aoai-service-name-12" {
+  type = string
+  description = "Name of the second AOAI service in Prority 1 group"
+  default = "can-aoai-smart-12"
+  
+}
+
+variable "aoai-service-name-21" {
+  type = string
+  default = "can-aoai-smart-21"
+  description = "Name of the first AOAI service in Prority 2 group"
+  
+}
+
+variable "resource_group_name" {
+  type = string
+  description = "Name of the resource group"
+  default = "can-aoai-smart-load"
+  
+}
+
+# Create resource group
+resource "azurerm_resource_group" "aoai-smart-load" {
+  name     = var.resource_group_name
+  location = "eastus"
+}
+
+# Create Application Inisghts for use with APIM
+resource "azurerm_application_insights" "aoai-app-insights" {
+  name                = "can-aoai-app-insights"
+  location            = azurerm_resource_group.aoai-smart-load.location
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+  application_type    = "web"
+}
+
+# Azure API Management
+resource "azurerm_api_management" "apim" {
+  name                = "can-aoai-smart-apim"
+  location            = azurerm_resource_group.aoai-smart-load.location
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+  publisher_email     = "test@contoso.com"
+  publisher_name      = "Contoso Ltd."
+  sku_name            = "Developer_1"
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Create APIM Logger
+resource "azurerm_api_management_logger" "apim-logger" {
+  name                = "apimlogger"
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+
+  application_insights {
+    instrumentation_key = azurerm_application_insights.aoai-app-insights.instrumentation_key
+  }
+}
+
+# Create APIM Diagnostic
+resource "azurerm_api_management_diagnostic" "apim-diag" {
+  identifier               = "applicationinsights"
+  resource_group_name      = azurerm_resource_group.aoai-smart-load.name
+  api_management_name      = azurerm_api_management.apim.name
+  api_management_logger_id = azurerm_api_management_logger.apim-logger.id
+
+  sampling_percentage       = 5.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "verbose"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  frontend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+
+  backend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  backend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+}
+
+# Create AOAI Priority 1 group
+# AOAI service 1-1
+resource "azurerm_cognitive_account" "aoai-smart-11" {
+  name                = var.aoai-service-name-11
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+  kind                = "OpenAI"
+  sku_name = "S0"
+  custom_subdomain_name = var.aoai-service-name-11
+}
+
+resource "azurerm_cognitive_deployment" "gpt-35-turbo-11" {
+  name                 = "gpt-35-turbo"
+  cognitive_account_id = azurerm_cognitive_account.aoai-smart-11.id
+  model {
+    format  = "OpenAI"
+    name    = "gpt-35-turbo"
+    version = "0613"
+  }
+
+  scale {
+    type = "Standard"
+  }
+}
+
+# AOAI service 1-2
+resource "azurerm_cognitive_account" "aoai-smart-12" {
+  name                = var.aoai-service-name-12
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+  kind                = "OpenAI"
+  sku_name = "S0"
+  custom_subdomain_name = var.aoai-service-name-12
+}
+
+resource "azurerm_cognitive_deployment" "gpt-35-turbo-12" {
+  name                 = "gpt-35-turbo"
+  cognitive_account_id = azurerm_cognitive_account.aoai-smart-12.id
+  model {
+    format  = "OpenAI"
+    name    = "gpt-35-turbo"
+    version = "0613"
+  }
+
+  scale {
+    type = "Standard"
+  }
+}
+
+# Create AOAI Priority 2 group
+# AOAI service 2-1
+resource "azurerm_cognitive_account" "aoai-smart-21" {
+  name                = var.aoai-service-name-21
+  location            = "canadaeast"
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+  kind                = "OpenAI"
+  sku_name = "S0"
+  custom_subdomain_name = var.aoai-service-name-21
+}
+
+resource "azurerm_cognitive_deployment" "gpt-35-turbo-21" {
+  name                 = "gpt-35-turbo"
+  cognitive_account_id = azurerm_cognitive_account.aoai-smart-21.id
+  model {
+    format  = "OpenAI"
+    name    = "gpt-35-turbo"
+    version = "0613"
+  }
+
+  scale {
+    type = "Standard"
+  }
+}
+
+# Give Azure APIM access to the all AOAI services
+resource "azurerm_role_assignment" "aoai11" {
+  scope                = azurerm_cognitive_account.aoai-smart-11.id
+  role_definition_name = "Cognitive Services OpenAI User"
+  principal_id         = azurerm_api_management.apim.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "aoai12" {
+  scope                = azurerm_cognitive_account.aoai-smart-12.id
+  role_definition_name = "Cognitive Services OpenAI User"
+  principal_id         = azurerm_api_management.apim.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "aoai21" {
+  scope                = azurerm_cognitive_account.aoai-smart-21.id
+  role_definition_name = "Cognitive Services OpenAI User"
+  principal_id         = azurerm_api_management.apim.identity[0].principal_id
+}
+
+# Create API in APIM
+resource "azurerm_api_management_api" "smart-api" {
+  name                = "aoai-smart-api"
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+  api_management_name = azurerm_api_management.apim.name
+  revision            = "1"
+  display_name        = "aoai-smart-api"
+  path                = "smart-api/openai"
+  protocols           = ["https"]
+
+  import {
+    content_format = "openapi"
+    content_value  = file("${path.module}/inference-2023-12-01-preview.json")
+  }
+
+  subscription_key_parameter_names {
+    header = "api-key"
+    query = "subscription-key"
+  }
+}
+
+# Edit XML to include above AOAI configuration
+data "template_file" "apim-policy" {
+  template = file("${path.module}/apim-policy.xml")
+  vars = {
+    aoai-service-name-11 = azurerm_cognitive_account.aoai-smart-11.endpoint
+    aoai-service-name-12 = azurerm_cognitive_account.aoai-smart-12.endpoint
+    aoai-service-name-21 = azurerm_cognitive_account.aoai-smart-21.endpoint
+  }
+}
+
+# Apply smart load balancing policy
+resource "azurerm_api_management_api_policy" "smart-policy" {
+  api_name            = azurerm_api_management_api.smart-api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.aoai-smart-load.name
+
+  xml_content = "${data.template_file.apim-policy.rendered}"
+}
